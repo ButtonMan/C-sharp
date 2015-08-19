93c6f3c8e7b19c38b1fc7e33dc5246d040f4d241
Евгений Мирошниченко <mev-w@ukr.net>
2015-08-19 16:20:54 +0300

    Classes and Objects
    Задание 1. Создать класс Point, содержащий следующие члены класса:
    1. Поля:
    int x, y;
    2. Конструкторы, позволяющие создать экземпляр класса:
    -с нулевыми координатами;
    -с заданными координатами.
    3. Методы, позволяющие:
    -вывести координаты точки на экран;
    -рассчитать расстояние от начала координат до точки;
    -переместить точку на плоскости на вектор (a, b).
    4. Свойство:
    -позволяющее получить-установить координаты точки (доступное для чтения
    и
    записи);
    -позволяющее умножить координаты точки на скаляр (доступное только для
    записи).
    5. Индексатор, позволяющий по индексу 0 обращаться к полю x, по индексу
    1 – к
    полю y, при других значениях индекса выдается сообщение об ошибке.
    6. Перегрузку:
    -операции ++ (--): одновременно увеличивает (уменьшает) значение полей х
    и у
    на 1;
    -констант true и false: обращение к экземпляру класса дает значение
    true, если
    значение полей x и у совпадает, иначе false;
    -операции бинарный +: одновременно добавляет к полям х и у значение
    скаляра.
    Продемонстрировать работу класса.

---
+++ Homeworks/MicrosoftVirtualAcademy/Classes-and-Objects/Hometask-1/Hometask-1/Point.cs
@@ -0,0 +1,252 @@
+﻿//+---------------------------------------------------------------------------+
+/*  КЛАСС Point
+ *  
+ *  Предназначение: Хранение значений координат точки. Обеспечение возможности
+ *  редактирования и извличения значений координат точки. Выполнение некоторых
+ *  расчетов связанных с координатой
+ *  
+ */
+//+---------------------------------------------------------------------------+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+//ОБЪЯВЛЕНИЕ ПРОСТРАНСТВА ИМЕН
+//+---------------------------------------------------------------------------+
+namespace Hometask_1
+{
+
+
+
+    //ОБЪЯВЛЕНИЕ КЛАССА
+    //+-----------------------------------------------------------------------+
+    class Point
+    {
+
+
+
+        //ЧЛЕНЫ-ДАННЫЗ КЛАССА
+        //+-------------------------------------------------------------------+
+        private int x;          //значение по оси X
+        private int y;          //значение по оси Y
+
+
+
+        //КОНСТРУКТОРЫ КЛАССА
+        //+-------------------------------------------------------------------+
+       
+        /// <summary>конструктор без параметров</summary>
+        public Point()
+        {
+            this.x = 0;
+            this.y = 0;
+        }
+
+        /// <summary>конструктор инициализирующий значение координат точки по 
+        /// оси Х, У</summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+
+
+        //СВОЙСТВА КЛАССА
+        //+-------------------------------------------------------------------+
+
+        /// <summary> редактирования положения точки по оси Х </summary>
+        public int X
+        {
+            get
+            {
+                return this.x;
+            }
+            set
+            {
+                this.x = value;
+            }
+        }
+
+        /// <summary> редактирования положения точки по оси Y </summary>
+        public int Y
+        {
+            get
+            {
+                return this.y;
+            }
+            set
+            {
+                this.y = value;
+            }
+        }
+
+        /// <summary> умножение координаты точки на скаляр </summary>
+        public int MultiplyScalX
+        {
+            set
+            {
+                this.X *= value;
+            }
+        }
+
+        /// <summary> умножение координаты точки на скаляр </summary>
+        public int MultiplyScalY
+        {
+            set
+            {
+                this.Y *= value;
+            }
+        }
+
+
+
+        //ИНДЕКСАТОРЫ КЛАССА
+        //+-------------------------------------------------------------------+
+
+        /// <summary> Индексатор, позволяющий по индексу 0 обращаться к 
+        /// полю x, по индексу 1 – к полю y, при других значениях индекса 
+        /// выдается сообщение об ошибке </summary>
+        /// <param name="index">индекс элемента</param>
+        public int this[int index]
+        {
+            set
+            {
+                switch(index)
+                {
+                    case 0:
+                        this.X = value;
+                        break;
+                    case 1:
+                        this.Y = value;
+                        break;
+                    default:
+                        Console.WriteLine("!!!Wrong index!!!");
+                        break;
+                }
+            }
+        }
+
+
+
+        //МЕТОДЫ КЛАССА
+        //+-------------------------------------------------------------------+
+
+        /// <summary>вывод координаты точки на экран</summary>
+        public void Show()
+        {
+            Console.WriteLine("({0},{1})", this.x, this.y);
+        }
+
+        /// <summary> расчет расстояния от точки до начала 
+        /// координат</summary>
+        /// <returns> расстояни от точки до начала координат </returns>
+        public double VectorDistance()
+        {
+            return Math.Sqrt(Math.Pow(this.x, 2) + Math.Pow(this.y, 2));
+        }
+
+        /// <summary> суммирование векторов </summary>
+        /// <param name="a">координата по оси Х</param>
+        /// <param name="b">координата по оси У</param>
+        /// <returns>сумма векторов</returns>
+        public Point VectorSum(int a, int b)
+        {
+            return new Point(this.x += a, this.y += b);
+        }
+
+
+
+        //ПЕРЕГРУЖЕННЫЕ ОПЕРАТОРЫ КЛАССА
+        //+-------------------------------------------------------------------+
+
+        /// <summary> перегруженный префиксный оператор ++ </summary>
+        /// <param name="point">объект класса точка</param>
+        /// <returns>объект класса точка с увеличенным на единицу значением
+        /// координат</returns>
+        public static Point operator++(Point point)
+        {
+            ++point.X;
+            ++point.Y;
+            return point;
+        }
+
+        /// <summary> перегруженный префиксный оператор -- </summary>
+        /// <param name="point">объект класса точка</param>
+        /// <returns>объект класса точка с уменьшенным на единицу значением
+        /// координат</returns>
+        public static Point operator--(Point point)
+        {
+            --point.X;
+            --point.Y;
+            return point;
+        }
+
+        /// <summary>перегрузка логической константы true. Обращение к 
+        /// экземпляру класса дает значение true, если значение полей x и у 
+        /// совпадает, иначе false</summary>
+        /// <param name="point">объект класса точка</param>
+        /// <returns>логическая константа true или false </returns>
+        public static bool operator true(Point point)
+        {
+            if(point.X == point.Y)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>перегрузка логической константы false. Обращение к 
+        /// экземпляру класса дает значение true, если значение полей x и у 
+        /// совпадает, иначе false</summary>
+        /// <param name="point">объект класса точка</param>
+        /// <returns>логическая константа true или false </returns>
+        public static bool operator false(Point point)
+        {
+            if(point.X != point.Y)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        /// <summary>перегрузка бинарного оператора +, одновременно добавляет 
+        /// к полям х и у значение скаляра </summary>
+        /// <param name="obj">объект класса точка</param>
+        /// <param name="scal">значение скаляра</param>
+        /// <returns>объект класса точка</returns>
+        public static Point operator +(Point point, int scal)
+        {
+            point.x += scal;
+            point.y += scal;
+            return point;
+        }
+
+        /// <summary>перегрузка бинарного оператора +, одновременно добавляет 
+        /// к полям х и у значение скаляра </summary>
+        /// <param name="scal">объект класса точка</param>
+        /// <param name="obj">значение скаляра</param>
+        /// <returns>объект класса точка</returns>
+        public static Point operator +(int scal, Point point)
+        {
+            point.x += scal;
+            point.y += scal;
+            return point;
+        }
+    }
+}
---
+++ Homeworks/MicrosoftVirtualAcademy/Classes-and-Objects/Hometask-1/Hometask-1/Program.cs
@@ -0,0 +1,173 @@
+﻿//+---------------------------------------------------------------------------+
+/*Задание 1. Создать класс Point, содержащий следующие члены класса:
+ * 1. Поля:
+ * int x, y;
+ * 2. Конструкторы, позволяющие создать экземпляр класса: 
+ * с нулевыми координатами;
+ * с заданными координатами.
+ * 3. Методы, позволяющие:
+ * вывести координаты точки на экран;
+ * рассчитать расстояние от начала координат до точки;
+ * переместить точку на плоскости на вектор (a, b).
+ * 4. Свойство:
+ * позволяющее получить-установить координаты точки (доступное для чтения и
+ * записи);
+ * позволяющее умножить координаты точки на скаляр (доступное только для
+ * записи).
+ * 5. Индексатор, позволяющий по индексу 0 обращаться к полю x, по индексу 1 – к
+ * полю y, при других значениях индекса выдается сообщение об ошибке.
+ * 6. Перегрузку:
+ * операции ++ (--): одновременно увеличивает (уменьшает) значение полей х и у
+на 1;
+ * констант true и false: обращение к экземпляру класса дает значение true, если
+значение полей x и у совпадает, иначе false;
+ * операции бинарный +: одновременно добавляет к полям х и у значение скаляра.
+ * Продемонстрировать работу класса.
+ */
+//+---------------------------------------------------------------------------+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+//ОБЪЯВЛЕНИЕ ПРОСТРАНСТВА ИМЕН
+//+---------------------------------------------------------------------------+
+namespace Hometask_1
+{
+
+
+    class Program
+    {
+
+        //ТОЧКА ВХОДА В ПРОГРАММУ
+        //+-----------------------------------------------------------------------+
+        static void Main(string[] args)
+        {
+
+
+
+            //::создание объекта Point с использованием конструктора без параметров
+            Point pointA = new Point();
+            Console.Write("1. Разультат работы конструктора без параметров " +
+                Environment.NewLine + "--> pointA");
+            pointA.Show();
+            Console.WriteLine();
+
+
+
+            //::создание объекта Point с использованием конструктора с параметрами
+            Point pointB = new Point(1, 2);
+            Console.Write("2. Результат работы конструктора с параметрами " +
+                Environment.NewLine + "--> pointB");
+            pointB.Show();
+            Console.WriteLine();
+
+
+
+            //::тестирование свойства позволяющего редактировать и извлекать 
+            //  положение точки по Х 
+            Console.WriteLine("3. Tестирование свойств позволяющих редактировать"
+                + " и извлекать положение точки по оси Х, Y");
+            pointA.X = 2;
+            Console.WriteLine("pointA.X = {0}", pointA.X);
+            pointA.Y = 4;
+            Console.WriteLine("pointA.Y = {0}", pointA.Y);
+            Console.Write("pointA");
+            pointA.Show();
+            Console.WriteLine();
+
+
+
+            //::Тестирование свойства выполняющего умножать  координату 
+            //  точки на  скаляр
+            Console.WriteLine("4. Тестирование свойств выполняющих " +
+                "умножение координаты точки на скаляр");
+            Console.WriteLine("pointA.MultiplyScalX = {0}", 2);
+            Console.WriteLine("pointA.MultiplyScalY = {0}", 2);
+            pointA.MultiplyScalX = 2;
+            pointA.MultiplyScalY = 2;
+            Console.Write("pointA");
+            pointA.Show();
+            Console.WriteLine();
+
+
+
+            //::расчет расстояния от точки до начала координат
+            Console.WriteLine("5. Расчет расстояния от точки до начала " +
+                "координат");
+            Console.WriteLine("pointA.VectorDistance --> {0}", 
+                pointA.VectorDistance());
+            Console.WriteLine();
+
+
+
+            //::выполнение суммирования векторов
+            Console.WriteLine("6. Тестирование операции суммирования векторов");
+            Point pointC = new Point(2, 4);
+            Console.Write("pointC({0},{1}) + pointA({2},{3}) =",pointC.X,
+                pointC.Y, pointA.X, pointA.Y);
+            pointA.VectorSum(pointC.X, pointC.Y);
+            pointA.Show();
+            Console.WriteLine();
+
+
+
+            //::Тестирование работы индексатора, позволяющего по индексу 0 
+            //  обращаться к полю x, по индексу 1 – к полю y, при других 
+            //  значениях индекса выдается сообщение об ошибке
+            Console.WriteLine("7. Тестирование работы индексатора, " +
+                "позволяющего по индексу 0 обращаться к полю x, по индексу 1" +
+                "– к полю y, при других значениях индекса выдается сообщение" +
+                "об ошибке");
+            Console.WriteLine("pointA[0] = {0}", pointA[0] = 1);
+            Console.WriteLine("pointA[1] = {0}", pointA[1] = 2);
+            Console.Write("pointA");
+            pointA.Show();
+            Console.WriteLine();
+
+
+
+            //::Тестирование перегруженного унарного оператора ++
+            Console.WriteLine("8. Тестирование перегруженного унарного " +
+                "оператора ++");
+            ++pointA;
+            Console.WriteLine("++pointA = ({0},{1})", pointA.X, pointA.Y);
+            Console.WriteLine();
+
+
+
+
+            //::Тестирование перегруженного унарного оператора ++
+            Console.WriteLine("9. Тестирование перегруженного унарного " +
+                "оператора ++");
+            --pointA;
+            Console.WriteLine("++pointA = ({0},{1})", pointA.X, pointA.Y);
+            Console.WriteLine();
+
+
+
+            //::Тестирование перегруженных констант true и false
+            Console.WriteLine("10. Тестирование перегруженных констант" +
+                "true и false");
+            Console.WriteLine("(pointA({0},{1})? true : false = {2}", pointA.X, 
+                pointA.Y, (pointA)? true : false);
+            Console.WriteLine();
+
+
+            //::Тестирование работы перегруженного бинарного оператора +
+            Console.WriteLine("11. Тестирование работы перегруженного " +
+            "бинарного оператора \"+\"");
+            Console.Write("a) pointA({0},{1}) + 1 = ", pointA.X, pointA.Y);
+            pointA = pointA + 1;
+            Console.WriteLine("pointA({0},{1})", pointA.X, pointA.Y);
+            Console.Write("b) 1 + pointA({0},{1}) = ", pointA.X, pointA.Y);
+            Point A = 1 + pointA;
+            Console.WriteLine("pointA({0},{1})", pointA.X, pointA.Y);
+            Console.WriteLine();
+        }
+    }
+}
