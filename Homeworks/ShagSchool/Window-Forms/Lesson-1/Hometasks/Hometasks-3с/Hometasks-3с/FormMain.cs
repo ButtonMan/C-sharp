b600df2530ac2ba622b7a31bddbcd9cbe2421cc7
Евгений Мирошниченко <mev-w@ukr.net>
2015-09-19 00:18:21 +0300

    Условие задачи:
    УСЛОВИЕ ЗАДАЧИ:
    
    Представьте, что у вас на форме есть прямоугольник, границы которого на
    10 пикселей отстоят от границ рабочей области формы. Необходимо создать
    следующие обработчики:
    c. Обработчик перемещения указателя мыши в пределах рабочей области,
    который должен выводить в заголовок окна текущие координаты мыши x и y.

---
+++ Homeworks/ShagSchool/Window-Forms/Lesson-1/Hometasks/Hometasks-3с/Hometasks-3с/FormMain.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hometasks_3c
+{
+    /// <summary>
+    /// Главное окно программы
+    /// </summary>
+    class FormMain : Form
+    {
+        /// <summary>
+        /// Конструкто главного окна программы
+        /// </summary>
+        public FormMain()
+        {
+            this.FormMainInitialization();
+        }
+
+        /// <summary>
+        /// Создает главное окно программы 
+        /// </summary>
+        private void FormMainInitialization()
+        {
+            this.ClientSize = new Size(400, 400);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Text = "Задание №3c";
+            this.BackColor = Color.DarkSlateGray;
+            this.ForeColor = Color.White;
+            this.FormBorderStyle = FormBorderStyle.Sizable;
+            this.MouseMove += new MouseEventHandler(FormMainMouseMove);
+        }
+
+        /// <summary>
+        /// Обработчик события "движение мыши".
+        /// Выводит в заголовок координаты мыши.
+        /// </summary>
+        private void FormMainMouseMove(Object sender, MouseEventArgs e)
+        {
+            this.Text = "Координаты мыши: X =  " + e.X + " Y = " + e.Y;
+        }
+    }
+}
---
+++ Homeworks/ShagSchool/Window-Forms/Lesson-1/Hometasks/Hometasks-3с/Hometasks-3с/Program.cs
@@ -0,0 +1,36 @@
+﻿//+---------------------------------------------------------------------------+
+//УСЛОВИЕ ЗАДАЧИ:
+/*
+    Представьте, что у вас на форме есть прямоугольник, границы которого 
+    на 10 пикселей отстоят от границ рабочей области формы. Необходимо 
+    создать следующие обработчики: 
+
+    c. Обработчик перемещения указателя мыши в пределах рабочей области, 
+    который должен выводить в заголовок окна текущие координаты мыши x и y. 
+*/
+//+---------------------------------------------------------------------------+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hometasks_3с
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
