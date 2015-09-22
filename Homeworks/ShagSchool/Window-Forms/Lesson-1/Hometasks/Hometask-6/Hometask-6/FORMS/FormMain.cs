de5a83331b7c0fbfbc5e0ee1f4a1aa8932633d79
Евгений Мирошниченко <mev-w@ukr.net>
2015-09-22 19:51:06 +0300

    Условие задачи:
    УСЛОВИЕ ЗАДАЧИ:
    Написать программу, которая по введенной дате определяет день недели.
    Результат выводить в текстовое поле (желательно по-русски).
    */

---
+++ Homeworks/ShagSchool/Window-Forms/Lesson-1/Hometasks/Hometask-6/Hometask-6/FORMS/FormMain.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Hometask_6.FORMS
+{
+    class FormMain : Form
+    {
+        System.Globalization.CultureInfo culture = new System.Globalization.CultureInfo("ru-RU");
+
+        //ДОЧЕРНИЕ КОНТРОЛЛЕРЫ КЛАССА
+        //+-------------------------------------------------------------------+
+        private Label labelDayWeekInfo = null;
+        private Label labelDayWeekResult = null;
+        private Label labelMonthCalendar = null;
+        private MonthCalendar monthCalendar = null;
+
+
+
+        //КОНСТРУКТОРЫ КЛАССА
+        //+-------------------------------------------------------------------+
+
+        /// <summary>
+        /// Cоздание окна и всех его дочерних контроллеров.
+        /// Подкиска обработчиков на события.
+        /// </summary>
+        public FormMain()
+        {
+            this.FormMainInitialization();
+            this.MonthCalendarInitialization();
+            this.LabelMonthCalendarInitialization();
+            this.LabelDayWeekInfoInitialization();
+            this.LabelDayWeekResultInitialization();
+            this.SetControls();
+            this.SetEvents();
+        }
+
+
+
+        //МЕТОДЫ КЛАССА
+        //+-------------------------------------------------------------------+
+
+        /// <summary>
+        /// Создание главного окна программы
+        /// </summary>
+        private void FormMainInitialization()
+        {
+            this.ClientSize = new Size(400, 400);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.MaximizeBox = false;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.BackColor = Color.DarkSlateGray;
+            this.ForeColor = Color.White;
+            this.Text = "Домашенее задание №6";
+        }
+
+        /// <summary>
+        /// Создание котноллера MonthCalendar
+        /// </summary>
+        private void MonthCalendarInitialization()
+        {
+            this.monthCalendar = new MonthCalendar();
+            this.monthCalendar.Anchor = AnchorStyles.Top | AnchorStyles.Right |
+                AnchorStyles.Bottom | AnchorStyles.Left;
+            int x = (this.ClientRectangle.Width - this.monthCalendar.Width) / 2;
+            int y = (this.ClientRectangle.Height - this.monthCalendar.Height) / 2;
+            this.monthCalendar.Location = new Point(x, y);
+        }
+
+        /// <summary>
+        /// Создание текстовой метки с сопроводительным текстом к контроллеру
+        /// MonthCalendar
+        /// </summary>
+        private void LabelMonthCalendarInitialization()
+        {
+            this.labelMonthCalendar = new Label();
+            this.labelMonthCalendar.AutoSize = true;
+            this.labelMonthCalendar.Text = "Выберите дату:";
+            int x = this.monthCalendar.Location.X;
+            int y = this.monthCalendar.Location.Y - 20;
+            this.labelMonthCalendar.Location = new Point(x, y);
+        }
+
+        /// <summary>
+        /// Создание текстовой метки с сопроводительным текстом к отображаемому названию дня недели
+        /// в метке  LabelDayWeekResul
+        /// </summary>
+        private void LabelDayWeekInfoInitialization()
+        {
+            this.labelDayWeekInfo = new Label();
+            int x = this.monthCalendar.Location.X;
+            int y = this.monthCalendar.Location.Y - 77;
+            this.labelDayWeekInfo.Location = new Point(x, y);
+            this.labelDayWeekInfo.Text = "День недели: ";
+            this.labelDayWeekInfo.Visible = false;
+            this.labelDayWeekInfo.Padding = new Padding(0);
+            this.labelDayWeekInfo.Margin = new Padding(0);
+        }
+
+        /// <summary>
+        /// Создание текстовой метки содержащей название дня недели выбранного 
+        /// пользователем с помощью контроллера MonthCalendar
+        /// </summary>
+        private void LabelDayWeekResultInitialization()
+        {
+            this.labelDayWeekResult = new Label();
+            this.labelDayWeekResult.AutoSize = true;
+            int x = this.monthCalendar.Left;
+            int y = this.monthCalendar.Top - 60;
+            this.labelDayWeekResult.Location = new Point(x, y);
+            this.labelDayWeekResult.Text = "СУББОТА";
+            this.labelDayWeekResult.Font = new Font(FontFamily.GenericSansSerif, 
+                20);
+            this.labelDayWeekResult.Visible = false;
+        }
+
+        /// <summary>
+        /// Присоединяет к главному окну программы контроллеры
+        /// </summary>
+        private void SetControls()
+        {
+            this.Controls.Add(this.monthCalendar);
+            this.Controls.Add(this.labelMonthCalendar);
+            this.Controls.Add(this.labelDayWeekInfo);
+            this.Controls.Add(this.labelDayWeekResult);
+        }
+
+        /// <summary>
+        /// Подписка на события обработчиков событий
+        /// </summary>
+        private void SetEvents()
+        {
+            this.monthCalendar.DateSelected += new DateRangeEventHandler(
+                MonthCalendarDateSelected);
+        }
+
+
+
+        //ОБРАБОТЧИК СОБЫТИЯ
+        //+-------------------------------------------------------------------+
+
+        /// <summary>
+        /// Обработка события "выбор даты" в календаре объекта класса 
+        /// MonthCalendar.
+        /// </summary>
+        private void MonthCalendarDateSelected(Object sender, EventArgs e)
+        {
+            this.labelDayWeekInfo.Visible = true;
+            this.labelDayWeekResult.Text = this.culture.DateTimeFormat.
+                GetDayName(this.monthCalendar.SelectionStart.DayOfWeek);
+            this.labelDayWeekResult.Visible = true;
+        }
+    }
+}
---
+++ Homeworks/ShagSchool/Window-Forms/Lesson-1/Hometasks/Hometask-6/Hometask-6/Program.cs
@@ -0,0 +1,32 @@
+﻿//+---------------------------------------------------------------------------+
+//УСЛОВИЕ ЗАДАЧИ:
+/*
+Написать программу, которая по введенной дате определяет день недели. 
+Результат выводить в текстовое поле (желательно по-русски). 
+*/
+//+---------------------------------------------------------------------------+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hometask_6
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Hometask_6.FORMS.FormMain());
+        }
+    }
+}
